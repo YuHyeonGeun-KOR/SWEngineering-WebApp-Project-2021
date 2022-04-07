3dd9348adade53b87a5a11aa609ae571b3f7d640
YuHyeonGeun-KOR <yuhyeonguen.dev@gmail.com>
2022-04-07 23:30:09 +0900

    Node Server: Register with HashSalt

--- server-side/routes/register.js
+++ server-side/routes/register.js
@@ -1,13 +1,10 @@
 const express = require('express');
+const { use } = require('.');
 const router = express.Router();
-const mysql = require('mysql');
-const dbConfig = require('./dbConfig');
-const dbOptions = {
-	user: dbConfig.user,
-	password: dbConfig.password,
-	database: dbConfig.database
-};
-const connection = mysql.createConnection(dbOptions);
+const bcrypt = require("bcrypt");
+const saltRounds = 10;
+const db = require('../models/index')
+require('dotenv').config();
 
 /* GET register method. */
 router.get('/', function (req, res, next) {
@@ -16,55 +13,75 @@ router.get('/', function (req, res, next) {
 
 
 /* POST register method. */
-router.post('/', function (req, res, next) {
-    const usercode = req.body.usercode;
+router.post('/', async(req, res) => {
+    let { userEmail, nickname, password, passwordCheck } = req.body;
 
-    const email = req.body.email;
-    const password = req.body.password;
-    const name = req.body.name;
-    const phone = req.body.phone;
-    const address = req.body.address;
+    // 빈값이 왔을 때 메시지
+    if (
+        userEmail === "" ||
+        nickname === "" ||
+        password === "" ||
+        passwordCheck === ""
+    ) {
+        return res.json({ registerSuccess: false, message: "정보를 입력하세요" });
+    }
 
-    if (usercode === "customer") {
-        const user_sql = "INSERT INTO tbl_customer(customer_email, customer_pwd, customer_name, customer_phone, customer_destination) VALUES (?, ?, ?, ?, ?)";
-        let user_data = [email, password, name, phone, address];
-        connection.query(user_sql, user_data, (err, results) => {
-            if (err) console.log(err);
-            console.log(results);
-            res.cookie("login", {
-                authorized: true,
-                id: results.insertId,
-                usercode: usercode
-            });
-            res.redirect('/');
+    if (password !== passwordCheck){
+        return res.json({
+            registerSuccess: false,
+            message: "입력하신 비밀번호가 일치하지 않습니다.",
         });
     }
-    else if (usercode === "company") {
-        const company_sql = "INSERT INTO tbl_company(company_email, company_pwd, company_name, company_tel, company_income) VALUES (?, ?, ?, ?, 1)";
-        const company_data = [email, password, name, phone];
-        connection.query(company_sql, company_data, (err, results) => {
-            if (err) console.log(err);
-            res.cookie("login", {
-                authorized: true,
-                id: results.insertId,
-                usercode: usercode
-            });
-            res.redirect('/');
+
+    const sameEmailChecker = await db.User.findOne({ userEmail: userEmail });
+    if (sameEmailChecker !== null){
+        return res.json({
+            registerSuccess : false,
+            message : "이미 등록된 이메일입니다.",
         });
     }
-    else {
-        const admin_sql = "INSERT INTO tbl_admin(admin_email, admin_pwd, admin_name) VALUES (?, ?, ?)";
-        const admin_data = [email, password, name];
-        connection.query(admin_sql, admin_data, (err, results) => {
-            if (err) console.log(err);
-            res.cookie("login", {
-                authorized: true,
-                id: results.insertId,
-                usercode: usercode
-            });
-            res.redirect('/');
-        });
+
+    const sameNickChecker = await db.User.findOne({ nickname : nickname});
+    if (sameNickChecker !== null){
+        return res.json({
+            registerSuccess : false,
+            message : "이미 등록된 별명입니다.",
+        })
     }
+
+    bcrypt.genSalt(saltRounds, (err, salt) => {
+        // 솔트 생성 실패시
+        if (err)
+          return res.status(500).json({
+            registerSuccess: false,
+            message: "해쉬화 하는 과정에서 오류가 발생했습니다.",
+          });
+        // salt 생성에 성공시 hash 진행
+    
+        bcrypt.hash(password, salt, async (err, hash) => {
+            if (err)
+                return res.status(500).json({
+                registerSuccess: false,
+                message: "비밀번호 해쉬화에 실패했습니다.",
+                });
+        
+            // 비밀번호를 해쉬된 값으로 대체합니다.
+            password = hash;
+        
+            const user = await db.User.create({
+                email : userEmail,
+                nickname,
+                password : password,
+            });
+                
+            user.save((err) => {
+                if (err) return res.json({ registerSuccess: fasle, message: err });
+            });
+            return res.json({ registerSuccess: true });
+            });
+    });
+    
+    
 });
 
 module.exports = router;
